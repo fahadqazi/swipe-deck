ca8508842bd5aa705599f5fa9b1195857c47a438
Fahad Qazi <fahadqazi@gmail.com>
2017-06-15 00:07:43 +0100

    used panResponder and animation to move components around on screen

--- src/Deck.js
+++ src/Deck.js
@@ -5,15 +5,16 @@ class Deck extends Component {
     constructor(props){
         super(props)
 
+        const position = new Animated.ValueXY();
         const panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: (event, gesture) => {
-                console.log(gesture);
+                position.setValue({ x:gesture.dx ,y:gesture.dy })
             },
             onPanResponderRelease: () => {}
         });
 
-        this.state = { panResponder };
+        this.state = { panResponder, position };
     }
     renderCards(){
         return this.props.data.map(item => {
@@ -22,9 +23,11 @@ class Deck extends Component {
     }
     render(){
         return(
-            <View {...this.state.panResponder.panHandlers}>
+            <Animated.View
+                style={this.state.position.getLayout()}
+                {...this.state.panResponder.panHandlers}>
                 {this.renderCards()}
-            </View>
+            </Animated.View>
         )
     }
 }
