5e7d0e602c02b532b534d1c7d3fe9fec1e385f9f
Fahad Qazi <fahadqazi@gmail.com>
2017-06-14 21:36:19 +0100

    added ball to page

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ball from './src/Ball';
 
 class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+              <Ball />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
 });
 
 Expo.registerRootComponent(App);
