c349f5aed22605eac9445acaf043769f189b3fd6
Fahad Qazi <fahadqazi@gmail.com>
2017-06-14 22:57:52 +0100

    adding interactions

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements'
 import Deck from './src/Deck';
 
 const DATA = [
@@ -15,10 +16,30 @@ const DATA = [
 ];
 
 class App extends React.Component {
+    renderCard(item){
+        return(
+            <Card
+                key={item.id}
+                title={item.text}
+                image={{ uri: item.uri}}
+            >
+                <Text style={{ marginBottom: 10 }}>I can customize the card further.</Text>
+
+                <Button
+                    icon={{ name: 'code' }}
+                    backgroundColor="#03A9F4"
+                    title="View Now"
+                />
+            </Card>
+        );
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Deck />
+                <Deck
+                    data={DATA}
+                    renderCard={this.renderCard}
+                />
             </View>
         );
     }
