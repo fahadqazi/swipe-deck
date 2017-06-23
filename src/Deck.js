eb01f6c3dcf8b712e6efd8b0916821aa296e9441
Fahad Qazi <fahadqazi@gmail.com>
2017-06-23 23:50:38 +0100

    using position interpolation to add rotation on drag

--- src/Deck.js
+++ src/Deck.js
@@ -17,11 +17,16 @@ class Deck extends Component {
     this.state = { panResponder, position };
   }
 
-  getCardStyle(){
-      return {
-        ...this.state.position.getLayout(),
-            transform: [{rotate: '45deg'}]
-      };
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-500, 0, 500],
+      outputRange: ['-120deg', '0deg', '120deg'],
+    });
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate: rotate }],
+    };
   }
 
   renderCards() {
