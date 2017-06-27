18ad54484f82886573be399636f0ceb231933c46
Fahad Qazi <fahadqazi@gmail.com>
2017-06-28 00:15:44 +0100

    refactor variable name

--- src/Deck.js
+++ src/Deck.js
@@ -69,8 +69,8 @@ class Deck extends Component {
   }
 
   renderCards() {
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
+    return this.props.data.map((item, i) => {
+      if (i === 0) {
         return (
           <Animated.View
             key={item.id}
