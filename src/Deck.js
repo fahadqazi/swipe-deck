48e8e907a56d4c513128895f2e0165e31fdc0557
Fahad Qazi <fahadqazi@gmail.com>
2017-06-28 22:29:04 +0100

    setting position absolute to stack images

--- src/Deck.js
+++ src/Deck.js
@@ -78,15 +78,19 @@ class Deck extends Component {
         return (
           <Animated.View
             key={item.id}
-            style={this.getCardStyle()}
+            style={[this.getCardStyle(), styles.cardStyle]}
             {...this.state.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
           </Animated.View>
         );
       }
-      return this.props.renderCard(item);
-    });
+      return (
+          <View key={item.id} style={styles.cardStyle}>
+              {this.props.renderCard(item)}
+          </View>
+      )
+    }).reverse();
   }
 
   render() {
@@ -95,6 +99,13 @@ class Deck extends Component {
         {this.renderCards()}
       </View>
     );
+  }
+}
+
+const styles = {
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH
   }
 }
 
